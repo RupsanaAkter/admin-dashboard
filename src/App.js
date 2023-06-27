7d2a12dfa58962426fc4440e08e97d7c4941ce41
R7558 <rupsanaworld1004@gmail.co>
2023-06-27 19:25:23 +0600

    added sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-// import Topbar from "./scenes/global/Topbar";
-// import Sidebar from "./scenes/global/Sidebar";
-// import Dashboard from "./scenes/dashboard";
+
 
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Dashboard from "./scenes/Dashboard/Dashboard";
+import Sidebar from "./scenes/Global/Sidebar";
 
 
 function App() {
@@ -18,7 +17,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          {/* <Sidebar isSidebar={isSidebar} /> */}
+          <Sidebar isSidebar={isSidebar} />
           <main className="content">
             {/* <Topbar setIsSidebar={setIsSidebar} /> */}
             <Routes>
