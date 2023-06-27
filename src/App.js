e1240b915b8039c669f9c8c3eb30cb6b61a53b2b
R7558 <rupsanaworld1004@gmail.co>
2023-06-27 19:02:02 +0600

    added dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import Topbar from "./scenes/global/Topbar";
-import Sidebar from "./scenes/global/Sidebar";
-import Dashboard from "./scenes/dashboard";
+// import Topbar from "./scenes/global/Topbar";
+// import Sidebar from "./scenes/global/Sidebar";
+// import Dashboard from "./scenes/dashboard";
 
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
+import Dashboard from "./scenes/Dashboard/Dashboard";
 
 
 function App() {
@@ -17,9 +18,9 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <Sidebar isSidebar={isSidebar} />
+          {/* <Sidebar isSidebar={isSidebar} /> */}
           <main className="content">
-            <Topbar setIsSidebar={setIsSidebar} />
+            {/* <Topbar setIsSidebar={setIsSidebar} /> */}
             <Routes>
               <Route path="/" element={<Dashboard />} />
               {/* <Route path="/team" element={<Team />} />
