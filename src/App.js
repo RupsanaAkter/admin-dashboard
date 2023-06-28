d3c25a397de8c0bafaf48a3f5effd90e75e2f0cc
R7558 <rupsanaworld1004@gmail.co>
2023-06-28 10:04:01 +0600

    added team

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { ColorModeContext, useMode } from "./theme";
 import Dashboard from "./scenes/Dashboard/Dashboard";
 import Sidebar from "./scenes/Global/Sidebar";
 import Topbar from "./scenes/Global/Topbar";
+import Team from "./scenes/Team/Team";
 
 
 function App() {
@@ -23,16 +24,16 @@ function App() {
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
-              <Route path="/calendar" element={<Calendar />} />
-              <Route path="/geography" element={<Geography />} /> */}
+               <Route path="/team" element={<Team />} /> 
+              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              {/* <Route path="/form" element={<Form />} /> */}
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/faq" element={<FAQ />} /> */}
+              {/* <Route path="/calendar" element={<Calendar />} /> */}
+              {/* <Route path="/geography" element={<Geography />} />  */}
             </Routes>
           </main>
         </div>
